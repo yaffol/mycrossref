a674e1688f2707cb2ea379302fd9861f6f6371a4
Patrick Vale <pvale@crossref.org>
2021-07-19 13:33:35 +0100

    Feat(AuthenticationMachine.ts): Adds Authentication State Machine

---
+++ src/statemachines/AuthenticationMachine.ts
@@ -0,0 +1,109 @@
+import { assign, createMachine, Sender } from 'xstate'
+
+interface UserDetails {
+  username: string;
+}
+
+export type AuthenticationMachineContext = {
+  userDetails?: UserDetails;
+};
+
+export type AuthenticationMachineEvent =
+  | {
+  type: 'REPORT_IS_LOGGED_IN';
+  userDetails: UserDetails;
+}
+  | {
+  type: 'REPORT_IS_LOGGED_OUT';
+}
+  | {
+  type: 'LOG_OUT';
+}
+  | {
+  type: 'LOG_IN';
+  userDetails: UserDetails;
+};
+
+const authenticationMachine = createMachine<
+  AuthenticationMachineContext,
+  AuthenticationMachineEvent
+  >(
+    {
+      id: 'authentication',
+      initial: 'checkingIfLoggedIn',
+      states: {
+        checkingIfLoggedIn: {
+          invoke: {
+            src: 'checkIfLoggedIn',
+            onError: {
+              target: 'loggedOut'
+            }
+          },
+          on: {
+            REPORT_IS_LOGGED_IN: {
+              target: 'loggedIn',
+              actions: 'assignUserDetailsToContext'
+            },
+            REPORT_IS_LOGGED_OUT: 'loggedOut'
+          }
+        },
+        loggedIn: {
+          on: {
+            LOG_OUT: {
+              target: 'loggedOut'
+            }
+          }
+        },
+        loggedOut: {
+          entry: ['navigateToAuthPage', 'clearUserDetailsFromContext'],
+          on: {
+            LOG_IN: {
+              target: 'loggedIn',
+              actions: 'assignUserDetailsToContext'
+            }
+          }
+        }
+      }
+    },
+    {
+      services: {
+        checkIfLoggedIn: () => async (
+          send: Sender<AuthenticationMachineEvent>
+        ) => {
+        // Perform some async check here
+          const isLoggedIn = false
+          if (isLoggedIn) {
+            send({
+              type: 'REPORT_IS_LOGGED_IN',
+              userDetails: {
+                username: 'demo_user@name.com'
+              }
+            })
+          } else {
+            send({
+              type: 'REPORT_IS_LOGGED_OUT'
+            })
+          }
+        }
+      },
+      actions: {
+        navigateToAuthPage: () => {
+        // When the user is logged out, we
+        // should take them to the /auth route
+        },
+        assignUserDetailsToContext: assign((context, event) => {
+          if (event.type !== 'REPORT_IS_LOGGED_IN' && event.type !== 'LOG_IN') {
+            return {}
+          }
+          return {
+            userDetails: event.userDetails
+          }
+        }),
+        clearUserDetailsFromContext: assign((context) => {
+          return { userDetails: undefined }
+        })
+      }
+    }
+  )
+
+export default authenticationMachine
