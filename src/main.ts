18905b8e9335bf8a080d369dc51e425f39388aed
Patrick Vale <pvale@crossref.org>
2021-08-19 10:17:30 +0100

    feat: Adds Mock Service Worker in dev mode

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import vuetify from './plugins/vuetify'
 import VueCompositionAPI from '@vue/composition-api'
 import { inspect } from '@xstate/inspect'
 import { useInspector } from '@/statemachines/utils'
+import { worker } from '@/mocks/browser'
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 // TODO: consider wrapping this in a test for "DEV mode"
 if (useInspector()) {
