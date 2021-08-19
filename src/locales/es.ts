4902c23ca32043076fd407fc31f72d1033cd6fda
Patrick Vale <pvale@crossref.org>
2021-08-19 15:11:45 +0100

    feat(i18n): Adds locales files and configures Vuetify to use them

---
+++ src/locales/es.ts
@@ -0,0 +1,12 @@
+import { es } from 'vuetify/src/locale'
+
+export default {
+  ...es,
+  key1: 'key1 cadena inglesa',
+  api_endpoint: ' El punto final de la API configurado es',
+  project_title: ' Marco Frontend',
+  open_dialog: ' diálogo abierto',
+  i_accept: 'Acepto',
+  privacy_policy_title: 'Política de privacidad',
+  language_menu_button: 'Idioma'
+}
