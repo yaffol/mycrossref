6dee830de82287db40a5d5997dd7345d972f5ff4
Patrick Vale <pvale@crossref.org>
2021-09-08 11:11:36 +0100

    chore: Adds urls constants, uses it in AppLogo, puts AppLogo in SystemHeaderBar

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-img src="https://assets.crossref.org/logo/crossref-logo-100.png" alt="Crossref logo" aspect-ratio="1.538"
+  <v-img :src=this.URL_LOGO_HORIZONTAL_RECTANGLE alt="Crossref logo" aspect-ratio="1.538"
          max-height="50px" max-width="72px" min-height="36px" min-width="72px"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
+import { URL_LOGO_HORIZONTAL_RECTANGLE } from '@/constants/urls'
 
 export default defineComponent({
-  name: 'AppLogo'
+  name: 'AppLogo',
+  setup () {
+    return {
+      // This is deliberately not reactive
+      URL_LOGO_HORIZONTAL_RECTANGLE
+    }
+  }
 })
 </script>
 
