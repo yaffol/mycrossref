e7c387b056f9709a804ad906d45233b812ba76b1
Patrick Vale <pvale@crossref.org>
2021-10-15 15:30:51 +0100

    build: disables eslint until .eslintrc 's can be ignored from .yalc/

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.VUE_APP_BASE_ROUTE ? process.env.VUE_APP_BASE_ROUTE : '/'
+  publicPath: process.env.VUE_APP_BASE_ROUTE ? process.env.VUE_APP_BASE_ROUTE : '/',
+  lintOnSave: false
 }
