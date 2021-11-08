15965be0d526d1b1b6dbb17d7ce77fd67e54bb20
Patrick Vale <pvale@crossref.org>
2021-11-08 11:32:21 +0000

    chore: removes example locales

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,128 +1,6 @@
 import get from 'lodash/get'
 import { en, de } from '@/locales/index'
 
-const enLocal = {
-  name: {
-    label: 'Name',
-    description: 'The name of the person'
-  },
-  vegetarian: {
-    label: 'Vegetarian',
-    description: 'Wether the person is a vegetarian'
-  },
-  birth: {
-    label: 'Birth Date',
-    description: ''
-  },
-  nationality: {
-    label: 'Nationality',
-    description: ''
-  },
-  'personal-data': {
-    age: {
-      label: 'Age'
-    },
-    driving: {
-      label: 'Driving Skill',
-      description: 'Indicating experience level'
-    }
-  },
-  height: {
-    label: 'Height'
-  },
-  occupation: {
-    label: 'Occupation',
-    description: ''
-  },
-  'postal-code': {
-    label: 'Postal Code'
-  },
-  form: {
-    grants: {
-      depositor: {
-        depositor_name: {
-          error: {
-            pattern: 'Please enter a name that matches a US telephone number'
-          }
-        },
-        email_address: {
-          error: {
-            customDONOTUSE: 'An email address is required to complete this form, you know',
-            format: 'This is not an email address'
-          }
-        },
-        error: {
-          required: 'You really must fill in this field in the Depositor section'
-        }
-      }
-    }
-  },
-  error: {
-    required: 'This globally scoped message tells you this field is required'
-  }
-}
-
-const deLocal = {
-  name: {
-    label: 'Name',
-    description: 'Der Name der Person'
-  },
-  vegetarian: {
-    label: 'Vegetarier',
-    description: 'Isst die Person vegetarisch?'
-  },
-  birth: {
-    label: 'Geburtsdatum',
-    description: ''
-  },
-  nationality: {
-    label: 'Nationalität',
-    description: '',
-    Other: 'Andere'
-  },
-  'personal-data': {
-    age: {
-      label: 'Alter'
-    },
-    driving: {
-      label: 'Fahrkenntnisse',
-      description: 'Fahrerfahrung der Person'
-    }
-  },
-  height: {
-    label: 'Größe'
-  },
-  occupation: {
-    label: 'Beruf',
-    description: ''
-  },
-  'postal-code': {
-    label: 'Postleitzahl'
-  },
-  form: {
-    grants: {
-      depositor: {
-        depositor_name: {
-          error: {
-            required: 'Hier ist Feldasbchribesaggen uber Depositor Name'
-          }
-        },
-        email_address: {
-          error: {
-            custom1: 'Schade falschgemmacksammen',
-            format: 'Hier ist keine Email',
-            required: 'Bitte ihren Email hier schriben'
-          }
-        }
-      }
-    }
-  },
-  error: {
-    required: 'Pflichtfeld'
-  },
-  'Additional Information': 'Zusätzliche Informationen'
-}
-
 export type SupportedLocales = 'en' | 'de'
 
 export const createTranslator =
@@ -130,5 +8,6 @@ export const createTranslator =
     (key: string, defaultMessage: string | undefined): string | undefined => {
       console.log('KEY IN TRANSLATOR:', key)
       console.log('DEFAULT MESSAGE IN TRANSLATOR:', defaultMessage)
-      return get(locale === 'en' ? en : de, key) ?? defaultMessage
+      const translation = get(locale === 'en' ? en : de, key) ?? defaultMessage
+      return translation || ''
     }
